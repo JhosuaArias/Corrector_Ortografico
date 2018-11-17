c6a2cce2cbfd862f7bbffa4527028e4778934320
JhosuaArias <jhosuarqui@hotmail.com>
2018-11-16 20:07:13 -0600

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        OrthographicCorrector orthographicCorrector = new OrthographicCorrector();
 
         String query = "Curzos de pithon gratis";
         System.out.println(query);
-        System.out.println(orthographicCorrector.correctOrthography(query));
+        double startTime = System.nanoTime();
+        System.out.println(OrthographicCorrector.correctOrthography(query));
+        double endTime = System.nanoTime();
+
+        System.out.println((endTime - startTime)/1000000);
+
+        query = "Me cago en la reputisima madre que la remilpario";
+        System.out.println(query);
+        System.out.println(OrthographicCorrector.correctOrthography(query));
     }
 }
--- src/OrthographicCorrector.java
+++ src/OrthographicCorrector.java
@@ -3,52 +3,95 @@ import javafx.util.Pair;
 import java.util.*;
 
 public class OrthographicCorrector {
-    private TreeMap<String,Integer> allWords;
+    private static TreeMap<String,Integer> allWords;
 
-    public OrthographicCorrector(){
-        this.allWords = new TreeMap<>();
-        FileHandler.getDictionaryWords(this.allWords);
-        FileHandler.getVocabularyWords(this.allWords);
-        System.out.println(this.allWords);
+    static {
+        allWords = new TreeMap<>();
+        FileHandler.getDictionaryWords(allWords);
+        FileHandler.getVocabularyWords(allWords);
     }
 
-    public String correctOrthography(String query){
-       LinkedHashMap<String,String> comparator = new LinkedHashMap<>();
+    public  OrthographicCorrector(){}
 
-        //Meto
+    /**
+     * This Method tries to correct it's terms
+     * @param query the query to correct
+     * @return a corrected query
+     */
+    public static String correctOrthography(String query){
+       LinkedHashMap<String,String> comparator = new LinkedHashMap<>();
+       query = query.toLowerCase();
+       //Put query into map
+       putQueryToMap(comparator,query);
+
+        ///Preprocess query
+        String purifiedQuery = Preprocessor.preprocess(query);
+
+        //Search Corrected terms in the query
+        searchCorrectedTerms(purifiedQuery, comparator);
+
+       // Remake the query with corrected terms and return it
+        return remakeQuery(comparator,query).trim();
+    }
+
+    /**
+     * This method puts a preprocessed query into a Map.
+     * @param map A map.
+     * @param query the query.
+     */
+    private static void putQueryToMap(LinkedHashMap<String,String> map, String query){
         String[] splitOriginalQuery = query.toLowerCase().split("\\s+");
 
         for (String word : splitOriginalQuery) {
-            comparator.put(word,"");
+            map.put(word,"");
         }
 
-        ///Preproceso
-        String purifiedQuery = Preprocessor.preprocess(query);
-
-        String[] splitQuery = purifiedQuery.split("\\s+");
-
-        for (String word : splitQuery) {
-            if(!this.allWords.containsKey(word)){
-                for (String vocabularyTerm : this.allWords.keySet()) {
-                    int distance = DistanceCalculator.computeLevenshteinDistance(vocabularyTerm,word);
-                    this.allWords.put(vocabularyTerm , distance);
-                }
-                comparator.put(word,entriesSortedByValues(this.allWords).first().getKey());
-            }
-        }
-
-        String correctedQuery = "";
-
-        for (String word : comparator.keySet()) {
-            if (comparator.get(word).compareTo("") == 0){
-                correctedQuery += word +" ";
-            }else{
-                correctedQuery += comparator.get(word) + " ";
-            }
-        }
-        return correctedQuery.trim();
     }
 
+    /**
+     * This Method search if a terms is in the vocabulary or a Spanish dictionary, if that's the case it ignores that term.
+     * Otherwise the method tries to search another word with a low distance.
+     * @param purifiedQuery The query with out stop words.
+     * @param map A map with the complete query
+     */
+    private static void searchCorrectedTerms(String purifiedQuery, LinkedHashMap<String, String> map){
+        String[] splitQuery = purifiedQuery.split("\\s+");
+        for (String word : splitQuery) {
+            if(!allWords.containsKey(word)){
+                for (String vocabularyTerm : allWords.keySet()) {
+                    int distance = DistanceCalculator.computeLevenshteinDistance(vocabularyTerm,word);
+                    allWords.put(vocabularyTerm , distance);
+                }
+                map.put(word,entriesSortedByValues(allWords).first().getKey());
+            }
+        }
+    }
+
+    /**
+     * This Method remakes a query to replace incorrect word with correct ones.
+     * @param map a map with the query
+     * @return A string with a correct query
+     */
+    private static String remakeQuery(LinkedHashMap<String, String> map, String originalQuery){
+        StringBuilder correctedQuery = new StringBuilder();
+        String [] splitQuery = originalQuery.split("\\s+");
+        for (String word : splitQuery) {
+            if (map.get(word).compareTo("") == 0){
+                correctedQuery.append(word).append(" ");
+            }else{
+                correctedQuery.append(map.get(word)).append(" ");
+            }
+        }
+        return correctedQuery.toString();
+    }
+
+    /**
+     * This method sorts a HashMap by the values
+     * @param map The map to sort
+     * @param <K> First Type
+     * @param <V> Second Type
+     * @return A sorted Set.
+     */
     static <K,V extends Comparable<? super V>>
     SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
         SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
